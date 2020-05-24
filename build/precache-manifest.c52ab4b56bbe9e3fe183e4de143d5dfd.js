fad438531635b5406c60f84e5ddb0e6c87455059
tsonge <tsonge44@gmail.com>
2020-05-24 03:35:52 -0400

    merging client and server directories

---
+++ build/precache-manifest.c52ab4b56bbe9e3fe183e4de143d5dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2f75d45b38cf610c773e61d20dc262",
+    "url": "/index.html"
+  },
+  {
+    "revision": "715126aa18ba954079e2",
+    "url": "/static/css/main.21804247.chunk.css"
+  },
+  {
+    "revision": "ca30634a36050530e3bb",
+    "url": "/static/js/2.e26e652a.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.e26e652a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715126aa18ba954079e2",
+    "url": "/static/js/main.767e37db.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
